b73100bbfe092031e8067f47ad8ac013e4733fd8
Kate Nech <katenech@gmail.com>
2016-03-18 14:33:28 +0300

    from Glech

---
+++ src/Simple.java
@@ -0,0 +1,13 @@
+
+public class Simple {
+
+	public static void main(String[] args) {
+		//1. здороваемся: add commit pull p
+		System.out.println("Привет от Е.В.");
+		
+		//2. прощаемся
+		
+		//можно поболтать
+	}
+
+}
