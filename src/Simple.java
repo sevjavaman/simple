0fcdcaa2e3958033e6c1ebe9d87cd196312ee8e4
Krokozyabra <arkachist@gmail.com>
2016-03-21 23:14:53 +0300

    from Chistiy

--- src/Simple.java
+++ src/Simple.java
@@ -4,10 +4,11 @@ public class Simple {
 	public static void main(String[] args) {
 		//1. здороваемся: add commit pull p
 		System.out.println("Привет от Е.В.");
-		
+		System.out.println("Привет от Аркадия!");
 		//2. прощаемся
-		
+		System.out.println("Пока от Аркадия!");
 		//можно поболтать
+		System.out.println("Это вопрос? Да, можно поболтать!");
 	}
 
 }
