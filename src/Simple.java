1dfd464ef124bc372f1ee34e1707549b6b9370f2
AndreyChertckov <andreychertckov@yandex.ru>
2016-03-22 18:26:59 +0400

    from Chertckov

--- src/Simple.java
+++ src/Simple.java
@@ -5,8 +5,10 @@ public class Simple {
 		//1. здороваемся: add commit pull p
 		System.out.println("Привет от Е.В.");
 		System.out.println("Привет от Аркадия!");
+		System.out.println("Привет от Андрия!");
 		//2. прощаемся
 		System.out.println("Пока от Аркадия!");
+		System.out.println("Пока от Андрея!");
 		//можно поболтать
 		System.out.println("Это вопрос? Да, можно поболтать!");
 	}
